1c34a5d2e26f4bdb996d9a872a671a9e5430d4d0
Freddy <fredhopelane@gmail.com>
2020-05-31 22:30:39 +0700

    refactor author model,  migration script and tests

--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -15,7 +15,7 @@ class AuthorManagementTest extends TestCase
     /** @test */
     public function an_authored_can_be_created()
     {
-        $this->withoutExceptionHandling();
+        //$this->withoutExceptionHandling();
 
         $this->post('/author', [
             'name' => 'Author Name',
